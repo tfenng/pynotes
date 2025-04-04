4d9c8a6fe2d96fb2264b15291dfcab40f3ce9391
tonyfenng <18113020130@163.com>
2025-04-04 12:59:32 +0800

    refactor

---  portsan.py
+++  portsan.py
@@ -14,8 +14,13 @@ def scan_ip_range(start_ip, end_ip, ports):
 
 if __name__ == '__main__':
     # 定义要扫描的 IP 地址范围和端口
-    start_ip = "192.168.1.10"
-    end_ip = "192.168.1.20"
+    start_ip = "192.168.2.1"
+    end_ip = "192.168.2.20"
     ports = [22, 23, 25, 80, 443]
+
+    if len(sys.argv)>2:
+        start_ip = sys.argv[1]
+        end_ip = sys.argv[2]
+    
     # 执行扫描
     scan_ip_range(start_ip, end_ip, ports)
